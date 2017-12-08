084f5c6a651eb9bf9fd62b2f90b8ce8e5e604ac9
JasonOnes <jasonr.jones14@gmail.com>
2017-12-08 10:05:41 -0800

    pythonanywhere db config

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Va:XXXXXXXXX@localhost:3306/Val-Aver'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Fund-Updater:steve123@localhost:3306/Fund-Updater'
  
+#Below config for db pythonanywhere
+"""
+ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="jasonones",
+    password="XXXXXXXXXXXXXXXXXXXX",
+    hostname="XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    databasename="jasonones$MupdateR",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
- 
 
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG'] = True
